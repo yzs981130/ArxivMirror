543f8b49042c524f35d7fb53aa5536d81cb44d34
叶志晟 <yzs981130@126.com>
2022-04-18 15:02:09 +0800

    Update arxiv.user.js

--- arxiv.user.js
+++ arxiv.user.js
@@ -2,17 +2,24 @@
 // @name         ArxivMirror
 // @namespace    https://github.com/yzs981130/ArxivMirror
 // @homepage     https://github.com/yzs981130/ArxivMirror
-// @version      0.0.1
+// @version      0.0.2
 // @author       yzs981130
 // @description  Arxiv mirror for personal use
 // @include      https://arxiv.org/abs/*
+// @grant        GM_setClipboard
+// @grant        GM_registerMenuCommand
 // @run-at       document-start
 // @license      MIT
 // ==/UserScript==
 
 window.onload=function() {
-  'use strict';
-  let mirror = 'arxiv.yezhisheng.me';
-  var ele = $(".full-text ul li a")[0];
+	'use strict';
+	let mirror = 'arxiv.yezhisheng.me';
+	var ele = $(".full-text ul li a")[0];
 	ele.href = ele.href.replace(/arxiv.org/g, mirror)+'.pdf';
+	
+	GM_registerMenuCommand('Copy link', function() {
+		const markdown = '[' + document.title + '](' + window.location.href + ')';
+		GM_setClipboard(markdown);
+	}, 'r');
 }
